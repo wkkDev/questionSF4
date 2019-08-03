aa32be70744e8534f40659675ede89213069f64d
Wojciech <42325562+wkkDev@users.noreply.github.com>
2019-08-03 20:06:34 +0200

    Update ContactType.php

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -1,13 +1,10 @@
 <?php
 
 namespace App\Form;
-
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
-
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -15,7 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ContactType extends AbstractType {
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+   public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('name', TextType::class, [
                 'attr' => ['class' => 'form-control']
