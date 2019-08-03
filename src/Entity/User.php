2c1b94a95bd1b524b28e1f89aedd0c7a8c390300
Wojciech <42325562+wkkDev@users.noreply.github.com>
2019-08-03 20:07:14 +0200

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,7 +2,6 @@
 // src/Entity/User.php
 
 namespace App\Entity;
-
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -10,8 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-class User extends BaseUser
-{
+class User extends BaseUser {
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
